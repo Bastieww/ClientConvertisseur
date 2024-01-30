c5312a96ff2cec9c6e1fc52cac18c05e277ef483
Bastien Vincent Dekens <bastien.vincent-dekens@etu.univ-smb.fr>
2024-01-30 11:47:33 +0100

    Ajoutez des fichiers projet.

---
+++ ClientConvertisseurV1/Models/Devise.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientConvertisseurV1.Models
+{
+    public class Devise
+    {
+        public int Id { get; set; }
+        public string NomDevise { get; set; }
+        public double Taux { get; set; }
+        public Devise()
+        {
+        }
+
+        public Devise(int id, string nomDevise, double taux)
+        {
+            Id = id;
+            NomDevise = nomDevise;
+            Taux = taux;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Devise devise &&
+                   Id == devise.Id &&
+                   NomDevise == devise.NomDevise &&
+                   Taux == devise.Taux;
+        }
+    }
+}
