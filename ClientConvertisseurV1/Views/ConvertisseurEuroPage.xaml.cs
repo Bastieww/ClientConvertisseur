10381260715f2097da2fd42b9ec646b7b3c75451
Bastien Vincent Dekens <bastien.vincent-dekens@etu.univ-smb.fr>
2024-01-31 01:33:35 +0100

    Données récupérees, mais pas affichées

--- ClientConvertisseurV1/Views/ConvertisseurEuroPage.xaml.cs
+++ ClientConvertisseurV1/Views/ConvertisseurEuroPage.xaml.cs
@@ -9,12 +9,14 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System.ComponentModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -24,8 +26,22 @@ namespace ClientConvertisseurV1.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ConvertisseurEuroPage : Page
+    public sealed partial class ConvertisseurEuroPage : Page, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<Devise> Devises { get; set; }
+
+        private Devise deviseSelectionnee;
+        public Devise DeviseSelectionnee
+        {
+            get { return deviseSelectionnee; }
+            set
+            {
+                deviseSelectionnee = value;
+                OnPropertyChanged("DeviseSelectionnee");
+            }
+        }
+     
         public ConvertisseurEuroPage()
         {
             this.InitializeComponent();
@@ -33,18 +49,29 @@ namespace ClientConvertisseurV1.Views
             GetDataOnLoadAsync();
         }
 
+       
+
         private async Task GetDataOnLoadAsync()
         {
             WSService service = new WSService("https://localhost:7088/api/");
-            List<Devise> result = await service.GetDevisesAsync("listeDevise");
-            //Ce n'est pas dans le TD, cherherz sur INternet (ou voir plus loin)
+            List<Devise> result = await service.GetDevisesAsync("devises");
+            
             if(result == null)
             {
-                MessageAsync("API non disponible !", "Erreur");
+                //MessageAsync("API non disponible !", "Erreur");
             }
             else
             {
-                Devises = new ObservableCollection<Devise>(result);
+                Devises = new ObservableCollection<Devise>(result); 
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler? handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
             }
         }
     }
